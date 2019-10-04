e8196966919550433687bbe591788fd2399c20bc
Deepak Tripathi <deepaktripathi20@gmail.com>
2019-10-03 20:05:45 -0700

    Palindrome

---
+++ MergeTwoSortedLists/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Nodes;
+
+namespace MergeTwoSortedLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers1 = { 1, 2, 4 };
+
+            ListNode dummy1 = new ListNode(0);
+            ListNode head1 = dummy1;
+
+            for (int i = 0; i <= numbers1.Length - 1; i++)
+            {
+                dummy1.next = new ListNode(numbers1[i]);
+                dummy1 = dummy1.next;
+            }
+
+            head1 = head1.next;
+
+            int[] numbers2 = { 1, 3, 4 };
+
+            ListNode dummy2 = new ListNode(0);
+            ListNode head2 = dummy2;
+
+            for (int i = 0; i <= numbers2.Length - 1; i++)
+            {
+                dummy2.next = new ListNode(numbers2[i]);
+                dummy2 = dummy2.next;
+            }
+
+            head2 = head2.next;
+
+            Program p = new Program();
+            ListNode returnNode = p.MergeTwoLists(head1, head2);
+
+         
+        }
+
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+
+            if (l1 == null) return l2;
+            if (l2 == null) return l1;
+            if (l1.val < l2.val)
+            {
+                l1.next = MergeTwoLists(l1.next, l2);
+                return l1;
+            }
+            else
+            {
+                l2.next = MergeTwoLists(l1, l2.next);
+                return l2;
+            }
+
+
+
+
+
+        }
+                
+    }
+}
---
+++ PalindromeLinkedList/Program.cs
@@ -0,0 +1,68 @@
+﻿using Nodes;
+using System;
+using System.Collections.Generic;
+
+namespace PalindromeLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = { 1, 2};
+           
+
+            ListNode dummy = new ListNode(0);
+            ListNode head = dummy;
+            int counter = 0;
+            while (counter <= numbers.Length - 1)
+            {
+               
+                ListNode node = new ListNode(numbers[counter]);
+                dummy.next = node;
+                dummy = dummy.next;
+                counter++;
+            }
+
+            head = head.next;
+
+            Program p = new Program();
+            Console.WriteLine(p.IsPalindrome(head));
+         
+        }
+
+
+        public bool IsPalindrome(ListNode head)
+        {
+
+            ListNode prev = null;
+            ListNode curr = null;
+            int cnt = 0;
+            List<int> objs = new List<int>();
+
+            while (head != null)
+            {
+                objs.Add(head.val);
+                curr = head.next;
+                head.next = prev;
+                prev = head;
+                head = curr;
+            }
+
+            head = prev;
+          
+            while (head != null)
+            {
+                if (objs[cnt] != head.val)
+                {
+                  
+                    return false;
+                }
+
+                head = head.next;
+                cnt++;
+            }
+            return true;
+        }
+    }
+}
