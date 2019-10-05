ea3971d1ec18a7c5d2433088e6be4a412f219f30
Deepak Tripathi <deepaktripathi20@gmail.com>
2019-10-04 19:38:10 -0700

    Updated question summary

--- RotateArray/Program.cs
+++ RotateArray/Program.cs
@@ -1,4 +1,29 @@
-﻿using System;
+﻿/*
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+==========================================
+Input: [1,2,3,4,5,6,7] and k = 3
+Output: [5,6,7,1,2,3,4]
+
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+==========================================
+Input: [-1,-100,3,99] and k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+
+Author:Deepak Tripathi
+Date  :10/04/2019
+*/
+
+using System;
 
 namespace RotateArray
 {
@@ -38,7 +63,6 @@ namespace RotateArray
                 start++;
                 end--;
             }
-
 
         }
     }
