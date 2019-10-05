f73cdf76398657cd1c4548fa49cd6ae9c1fba3d0
Deepak Tripathi <deepaktripathi20@gmail.com>
2019-10-04 19:36:02 -0700

    Rotate arrays

---
+++ BestTimeToBuyAndSellStock/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BestTimeToBuyAndSellStock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine(p.MaxProfit(new int[] { 7, 1, 5, 3, 6, 4 }));
+        }
+
+
+        public int MaxProfit(int[] prices)
+        {
+
+            int profit = 0;
+
+            for (int i = 1; i< prices.Length ; i++)
+            {
+
+                if (prices[i-1] < prices[i])
+                {
+                    profit =profit + prices[i] - prices[i - 1];
+                }
+            }
+
+            return profit;
+        }
+    }
+}
---
+++ RotateArray/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace RotateArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+            int k = 3;
+
+            Program p = new Program();
+            p.Rotate(numbers, k);
+
+        }
+
+
+        public void Rotate(int[] nums, int k)
+        {
+
+            k = k % nums.Length;
+
+            this.Reverse(nums, 0, nums.Length - k - 1);
+            this.Reverse(nums, nums.Length - k, nums.Length - 1);
+            this.Reverse(nums, 0, nums.Length - 1);
+
+        }
+
+        private void Reverse(int[] nums, int start, int end)
+        {
+            int temp = 0;
+            while (start < end)
+            {
+                temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }
+
+
+        }
+    }
+}
